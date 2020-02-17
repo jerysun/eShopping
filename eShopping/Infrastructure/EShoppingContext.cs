fd18375dc613faaffc59f04f45ccdae9aad753e9
Jerry Sun <jerysun007@hotmail.com>
2020-02-17 22:31:50 +0100

    first commit

---
+++ eShopping/Infrastructure/EShoppingContext.cs
@@ -0,0 +1,18 @@
+﻿using eShopping.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopping.Infrastructure
+{
+    public class EShoppingContext : DbContext
+    {
+        public EShoppingContext(DbContextOptions<EShoppingContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Page> Pages { get; set; }
+    }
+}
---
+++ eShopping/Models/Page.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopping.Models
+{
+    public class Page
+    {
+        public int Id { get; set; } //will be automatically recognized as Primary Key once migration starts
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Slug { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public int Sorting { get; set; }
+    }
+}
