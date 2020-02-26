aa46b9880860b23079882345b851956c3e6c40ce
Jerry Sun <jerysun007@hotmail.com>
2020-02-27 00:40:21 +0100

    PagesController and its view

--- eShopping/Areas/Admin/Controllers/PagesController.cs
+++ eShopping/Areas/Admin/Controllers/PagesController.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eShopping.Infrastructure;
+using eShopping.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eShopping.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class PagesController : Controller
     {
-        public string Index()
+        private readonly EShoppingContext _context;
+
+        public PagesController(EShoppingContext context)
         {
-            return "test";
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            // IQueryable<Page> pages = from p in _context.Pages orderby p.Sorting select p;
+            // IQueryable<Page> pages = _context.Pages.OrderBy(p => p.Sorting);
+            // List<Page> pagesList = await pages.ToListAsync();
+            List<Page> pagesList = await _context.Pages.OrderBy(p => p.Sorting).ToListAsync();
+            return View(pagesList);
         }
     }
 }
