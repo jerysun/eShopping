d51e612c6cfcfe50792a9149a039ab41e58ddbe8
Jerry Sun <jerysun007@hotmail.com>
2020-06-21 02:55:28 +0200

    Added Create for Category

---
+++ eShopping/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShopping.Infrastructure;
+using eShopping.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShopping.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly EShoppingContext _context;
+
+        public CategoriesController(EShoppingContext context)
+        {
+            _context = context;
+        }
+
+        // GET /admin/categories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.OrderBy(c => c.Sorting).ToListAsync());
+        }
+
+        // GET /admin/categories/create
+        public IActionResult Create() => View();
+
+        // POST /admin/categories/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.Slug = category.Name.ToLower().Replace(" ", "-");
+                category.Sorting = 100;
+
+                var slug = await _context.Categories.FirstOrDefaultAsync(c => c.Slug == category.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The category already exists");
+                    return View(category); // re-fill the correct information
+                }
+
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                TempData["Success"] = "The category has been added!";
+
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+    }
+}
--- eShopping/Infrastructure/EShoppingContext.cs
+++ eShopping/Infrastructure/EShoppingContext.cs
@@ -14,5 +14,6 @@ namespace eShopping.Infrastructure
         }
 
         public DbSet<Page> Pages { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
