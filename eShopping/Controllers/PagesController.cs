087a68ae186d8fd866d066de18bf32392f32c917
Jerry Sun <jerysun007@hotmail.com>
2020-06-30 01:19:03 +0200

    added PagesControoler and views

---
+++ eShopping/Controllers/PagesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShopping.Infrastructure;
+using eShopping.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShopping.Controllers
+{
+    public class PagesController : Controller
+    {
+        private readonly EShoppingContext _context;
+
+        public PagesController(EShoppingContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET / or /slug
+        public async Task<IActionResult> Page(string slug)
+        {
+            if (string.IsNullOrEmpty(slug))
+            {
+                return View(await _context.Pages.Where(p => p.Slug == "home").FirstOrDefaultAsync());
+            }
+
+            Page page = await _context.Pages.Where(p => p.Slug == slug).FirstOrDefaultAsync();
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
+        }
+    }
+}
---
+++ eShopping/Infrastructure/MainMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopping.Infrastructure
+{
+
+    public class MainMenuViewComponent : ViewComponent
+    {
+        private readonly EShoppingContext _context;
+
+        public MainMenuViewComponent(EShoppingContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var pages = await _context.Pages.OrderBy(p => p.Sorting).ToListAsync();
+
+            return View(pages); //Views/Shared/Components/{ViewComponent}/Default.cshtml
+        }
+    }
+}
