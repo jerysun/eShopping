20aa438a54d9d751f20ebddf85e03ec4fab018a1
Jerry Sun <jerysun007@hotmail.com>
2020-07-29 02:13:52 +0200

    added role edit

--- eShopping/Areas/Admin/Controllers/RolesController.cs
+++ eShopping/Areas/Admin/Controllers/RolesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using eShopping.Models;
 using Microsoft.AspNetCore.Identity;
@@ -53,26 +54,59 @@ namespace eShopping.Areas.Admin.Controllers
             return View();
         }
 
+        //GET /admin/roles/edit
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+
+            List<AppUser> members = new List<AppUser>();
+            List<AppUser> nonMembers = new List<AppUser>();
+
+            foreach (AppUser user in _userManager.Users)
+            {
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    members.Add(user);
+                }
+                else
+                {
+                    nonMembers.Add(user);
+                }
+            }
+
+            return View(new RoleEdit
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+        }
+
+        //POST /admin/roles/edit/roleEdit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(RoleEdit roleEdit)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result;
+                foreach (string userId in roleEdit.AddIds ?? new string[] { })
+                {
+                    AppUser user = await _userManager.FindByIdAsync(userId);
+                    result = await _userManager.AddToRoleAsync(user, roleEdit.RoleName);
+                }
+
+                foreach (string userId in roleEdit.DeleteIds ?? new string[] { })
+                {
+                    AppUser user = await _userManager.FindByIdAsync(userId);
+                    result = await _userManager.RemoveFromRoleAsync(user, roleEdit.RoleName);
+                }
+
+                return Redirect(Request.Headers["Referer"].ToString());
+            }
+
+            return View(roleEdit);
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
